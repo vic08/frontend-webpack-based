95a32885441f89547af4b240bc0810d0c8ec3010
vic <viktor@screencloud.io>
2018-02-12 19:24:17 +0700

    production build bug fix

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -31,7 +31,7 @@ const env = getClientEnvironment(publicUrl)
 
 // Assert this just to be safe.
 // Development builds of React are slow and not intended for production.
-if (env.stringified['process.env'].NODE_ENV !== '"production"') {
+if (env.stringified.NODE_ENV !== '"production"') {
   throw new Error('Production builds must have NODE_ENV=production.')
 }
 
